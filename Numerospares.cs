e27b8ff7e78ad8d6263d9d15b7c8ae7054ab7f34
larissarandrade14-alt <larissarandrade14@gmail.com>
2025-12-10 16:50:53 -0300

    Criar uma lógica que lê uma sequência de 10 números e mostra quantos deles são pares.

---
+++ Numerospares.cs
@@ -0,0 +1,20 @@
+namespace Ativ_pares;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=======Número Par=========");
+        int contagem = 0;
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"Digite o {i}º número: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                contagem++;
+            }
+
+        }
+        Console.WriteLine("A quantidade de números pares é: " + contagem);
+    }
+}
