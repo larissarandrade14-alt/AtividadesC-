95a57ff5bfbf3b1e78f16f02a7f411a2802eaee7
larissarandrade14-alt <larissarandrade14@gmail.com>
2025-12-11 09:55:41 -0300

    Criar uma lógica que soma todos os números de 1 até N, onde N é informado pelo usuário.

---
+++ Atividades/Atividade1.cs
@@ -0,0 +1,22 @@
+﻿//Criar uma lógica que soma todos os números de 1 até N, onde N é informado pelo usuário.
+
+using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        Console.WriteLine("===== Soma dos números =======");
+        Console.WriteLine("Digite um número: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int i = 0;
+        int soma = 0;
+        do
+        {
+            i++;
+            soma = soma + i;
+        }
+        while (i < N);
+        Console.WriteLine($"A soma de todos os números de 1 até {N} é: {soma}");
+    }
+}
